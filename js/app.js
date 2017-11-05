bd1f81a82051fcca7e72f5ed442e27ab51df162c
jchurch2112 <32181630+jchurch2112@users.noreply.github.com>
2017-11-04 18:58:20 -0700

    js files

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(document).foundation()
+
+
+$(document).foundation({
+  orbit: {
+    animation: 'slide',
+    timer_speed: 1000,
+    pause_on_hover: false,
+    animation_speed: 500,
+    navigation_arrows: true,
+    bullets: true
+  }
+});
